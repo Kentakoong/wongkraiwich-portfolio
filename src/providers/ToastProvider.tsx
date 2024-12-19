400eaf4703b49a6fa6cfbad09339eb97eba6cd1c
Wongkraiwich Chuenchomphu <wongkraiwich@inedible.dev>
2024-12-19 12:40:33 +0700

    fix: transparent toast

--- src/providers/ToastProvider.tsx
+++ src/providers/ToastProvider.tsx
@@ -16,13 +16,13 @@ export const ToastProvider = ({
           classNames: {
             toast:
               "dark:bg-neutral-800 text-neutral-900 dark:text-neutral-50 bg-neutral-200 flex items-center gap-x-2 p-4 rounded-xl shadow-lg w-full",
-            error:
-              "!bg-red-300/30 dark:!bg-red-700/30 text-red-900 dark:text-red-50",
+            title: "font-semibold",
+            error: "!bg-red-100 dark:!bg-red-900 text-red-900 dark:text-red-50",
             success:
-              "!bg-green-300/30 dark:!bg-green-700/30 text-green-900 dark:text-green-50",
+              "!bg-green-100 dark:!bg-green-900 text-green-900 dark:text-green-50",
             warning:
-              "!bg-yellow-300/30 dark:!bg-yellow-700/30 text-yellow-900 dark:text-yellow-50",
-            info: "!bg-blue-300/30 dark:!bg-blue-700/30 text-blue-900 dark:text-blue-50",
+              "!bg-yellow-100 dark:!bg-yellow-900 text-yellow-900 dark:text-yellow-50",
+            info: "!bg-blue-100 dark:!bg-blue-900 text-blue-900 dark:text-blue-50",
           },
         }}
       />
